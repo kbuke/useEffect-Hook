839d49a675e821ecefebe77a82d11aa26bad5719
Kaan Buke <kabuke13@gmail.com>
2025-09-22 11:33:37 +0200

    console logs when name and age values are changed.

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function Child() {
   const [age, setAge] = useState(0)
   const [name, setName] = useState("")
+
+  // console.log("Render") when the component re-renders
+
+  // console.log("Hi") when the component mounts
+  useEffect(() => {
+    console.log("Hi")
+  }, [])
+
+  // console.log("My name is....") when the name or age variables change
+  useEffect(() => {
+    console.log(`Hi my name is ${name} and I am ${age} years old.`)
+  }, [name, age])
 
   return (
     <div>
